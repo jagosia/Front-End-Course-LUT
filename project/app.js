786f9df7695b97e7333efad83d0218e13549aac2
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-04-27 17:01:40 +0300

    adding images + visual fixes

--- project/app.js
+++ project/app.js
@@ -1,3 +1,13 @@
+
+const showMoreBtn = document.getElementById('showMoreBtn');
+const extraCards = document.querySelectorAll('.extra-card');
+
+showMoreBtn.addEventListener('click', () => {
+    extraCards.forEach(card => card.style.display = 'block');
+    showMoreBtn.style.display = 'none';
+});
+
+
 document.querySelectorAll('.accordion-title').forEach(button => {
     button.addEventListener('click', () => {
         const item = button.parentElement;
